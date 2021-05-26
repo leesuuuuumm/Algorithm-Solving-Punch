7909842dc7e1882b8d10ca825453066371db6b18
tnalsvjscl <tnalsvjscl@naver.com>
2021-05-26 17:59:08 +0900

    add / algo / boj(10809_알파벳찾기)

---
+++ boj/Solution_boj_10809_알파벳찾기.java
@@ -0,0 +1,32 @@
+package com.ssafy;
+
+import java.util.Scanner;
+
+public class Solution_boj_10809_알파벳찾기 {
+
+	public static void main(String[] args) {
+	
+		Scanner scan=new Scanner(System.in);
+		
+		String s=scan.next();
+		char crr[]=new char[s.length()];
+		int arr[]=new int[26];
+		crr=s.toCharArray();
+		for (int i = 0; i <26; i++) {
+			
+			b:for (int j = 0; j <crr.length; j++) {
+				if(crr[j]==('a'+i)) {
+					arr[i]=j;
+					break b;
+				}
+				else {
+					arr[i]=-1;
+				}
+			}
+			
+		}
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+}
