2085349cc7618d60c5f38efd15e182859122347b
Sumin Lee <58407737+leesuuuuumm@users.noreply.github.com>
2021-06-25 16:58:39 +0900

    add / algo / boj(7568_덩치)

--- boj/Solution_7568_덩치.java
+++ boj/Solution_7568_덩치.java
@@ -3,7 +3,7 @@ package net.acmicpc;
 import java.util.Scanner;
 
 public class Solution_7568_덩치 {
-
+ 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
