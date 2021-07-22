3f7b5d9c5d8ece96f1a92bc6a19ff44602bdf291
tnalsvjscl <tnalsvjscl@naver.com>
2021-07-22 18:55:17 +0900

    programmers(제일작은수제거하기)

---
+++ programmers/Solution_제일작은수제거하기.java
@@ -0,0 +1,35 @@
+package com.programmers;
+
+import java.util.LinkedList;
+
+public class Solution_제일작은수제거하기 {
+	public static void main(String[] args) {
+		int arr[] = { 10 };
+		solution(arr);
+	}
+
+	public static LinkedList<Integer> solution(int[] arr) {
+		LinkedList<Integer> list = new LinkedList<Integer>();
+		int min = Integer.MAX_VALUE;
+
+		if (arr.length >= 1) {
+			for (int i = 0; i < arr.length; i++) {
+				min = Math.min(min, arr[i]);
+			}
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] == min)
+					continue;
+				else {
+					list.add(arr[i]);
+				}
+			}
+			if (list.isEmpty()) {
+				list.add(-1);
+			}
+
+		}
+		return list;
+
+	}
+
+}
