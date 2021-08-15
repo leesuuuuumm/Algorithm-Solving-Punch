54e72e0970da9e1c05d1168777b234c2014fa68f
tnalsvjscl <tnalsvjscl@naver.com>
2021-08-15 14:00:47 +0900

    add / algo / programmers(부족한금액계산하기_위클리1주차)

---
+++ programmers/Solution_부족한금액계산하기_위클리1주차.java
@@ -0,0 +1,18 @@
+package com.programmers;
+
+public class Solution_부족한금액계산하기_위클리1주차 {
+
+	public static void main(String[] args) {
+		solution(3, 20, 4);
+	}
+	public static long solution(int price, long money, int count) {
+		long change = 0;
+		for (int i = 1; i <= count; i++) {
+			change += (price * i);
+		}
+		if (change <= money)
+			return 0; 
+		else
+			return change - money;
+	}
+}
