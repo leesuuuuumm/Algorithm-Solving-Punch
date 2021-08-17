940870e1bb2424e4f0d92ee03a7f2efd378b2fe7
tnalsvjscl <tnalsvjscl@naver.com>
2021-08-17 23:11:47 +0900

    add / algo / programmers(내적)

---
+++ programmers/Solution_내적.java
@@ -0,0 +1,12 @@
+package com.programmers;
+
+public class Solution_내적 {
+	public int solution(int[] a, int[] b) {
+		int answer = 0;
+		for (int i = 0; i < a.length; i++) {
+			answer += (a[i] * b[i]);
+		}
+		return answer;
+	}
+
+}
